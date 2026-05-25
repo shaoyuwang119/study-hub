b6c987c9b48dafa15ffb8bbddf6f1ef59abc171c
Shaoyu Wang <shaoyuwang119@gmail.com>
2026-05-25 00:30:43 -0500

    redid changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { useEffect, useState } from 'react'
-import { Header, Sidebar, NoteCard } from './components'
+import { Rightbar, Sidebar, NoteCard } from './components'
 
-type Note = {
-  id: number
-  title: string
-  author: string
-  content_url: string
-}
+import type { Note } from './components/Types'
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([])
@@ -51,18 +46,23 @@ function App() {
     <div className="flex min-h-screen font-sans">
       <Sidebar />
 
-      <div className="flex flex-col gap-y-6 p-6 w-1/2 bg-zinc-50">
-        <div className="text-4xl font-medium text-zinc-800">Home</div>
+      <div className="flex-1 flex-col p-6 bg-zinc-50">
+        <div className="flex flex-row mb-4 justify-between">
+          <div className="text-4xl font-medium text-zinc-800">Home</div>
+          <button className="w-40 text-white px-4 py-2 rounded-md hover:shadow-2xl bg-blue-500 hover:bg-blue-600 cursor-pointer">
+            Upload Notes
+          </button>
+        </div>
 
-        <div className="space-y-2">
+        <div className="space-y-2 mb-2">
           <input
             placeholder="Search notes, classes, subjects..."
             className="w-full bg-white rounded-lg border border-gray-200 px-3 py-2 text-sm focus:outline-none focus:border-gray-300"
           />
           <div className="flex gap-x-4">
-            <button className="w-40 text-pink-500 p-2 rounded-md border border-pink-600 hover:bg-pink-100 cursor-pointer">
+            {/* <button className="w-40 text-pink-500 p-2 rounded-md border border-pink-600 hover:bg-pink-100 cursor-pointer">
               English
-            </button>
+            </button> */}
           </div>
         </div>
 
@@ -75,30 +75,13 @@ function App() {
               subject={note.title}
               author={note.author}
               preview={note.content_url}
-              saves={67}
+              saves={note.saves}
             />
           ))}
         </div>
-        <button className="w-50 text-white px-4 py-2 rounded-md hover:shadow-2xl bg-blue-500 hover:bg-blue-600 cursor-pointer">
-          Upload Notes
-        </button>
       </div>
 
-      <aside className="w-96 border-l border-gray-200 bg-white px-6 py-8">
-        <div className="text-2xl text-zinc-800">My saved</div>
-        <div className="flex flex-col items-start mt-4 gap-y-4">
-          {notes.map((note, index) => (
-            <NoteCard
-              key={index}
-              title={note.title}
-              subject={note.title}
-              author={note.author}
-              preview={note.content_url}
-              saves={67}
-            />
-          ))}
-        </div>
-      </aside>
+      <Rightbar notes={notes} />
     </div>
   )
 }
